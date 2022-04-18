3c1ac5d4afea6fdb151ebeb1c2861cdc2e7d7351
kellio23 <kellio23@uic.edu>
2022-04-18 18:47:16 -0500

    added map and comm areas

--- app.r
+++ app.r
@@ -1,4 +1,4 @@
-# written by kenan arica and kevin elliott for CS 424 proj3
+# written by kenan arica >:( and kevin elliott for CS 424 proj3
 
 library(shiny)
 library(shinydashboard)
@@ -10,6 +10,13 @@ library(scales)
 library(leaflet)
 library(data.table)
 library(plyr)
+
+## Kevin libraries for map
+# if this doesn't work on your side you might have to install sf and here
+library(sf)
+library(here)
+
+
 # load in our data
 start = proc.time()
 
@@ -58,11 +65,56 @@ zeta$minutes <- round(zeta$seconds / 60)
 # change limits of this X axis
 # ggplot(data=zeta, aes(zeta$minutes)) + geom_histogram(bins = 6, binwidth = 2,  fill="#098CF9") + ggtitle("Binned Length of Rides") + scale_y_continuous(labels = scales::comma) + scale_x_discrete(limits = c(0, 5, 10, 15, 20, 25)) + labs(x="Minutes elapsed", y = "Number of Rides") + scale_x_continuous()
 
+# Community area map code
+# commented out but if we wanted the data from the tsv its here, probably not needed though
+#commAreas <- read.table(file = "CommAreas.tsv", sep = "\t", header = TRUE, quote = "" , fill=TRUE, row.names = NULL)
+# this command only works if all 4 of the geo_export files are in the project even though it only uses the .shp one
+comm_areas <- here("geo_export_a2580528-8f69-4499-9852-36e55c978a3b.shp") %>%
+  st_read()
+
+# p is testing to see plot without map
+p <- ggplot(data = comm_areas)+ geom_sf()
+#p <- p + geom_point()
+#print(p)
+# printPlot is used for leaflet map
+printPlot <- st_transform(comm_areas)
+
+# Text display on community area Based off the Leaflet for R tutorial found at
+# https://rstudio.github.io/leaflet/choropleths.html
+labels <- sprintf(
+  "<strong>Community Area: %s</strong><br/> %s",
+  comm_areas$area_num_1, comm_areas$community
+) %>% lapply(htmltools::HTML)
+
 
 ui <- fluidPage(
-  "Hello, world!"
+  leafletOutput("map")
 )
 server <- function(input, output, session) {
+  output$map <- renderLeaflet({
+    #including dasharray in the highlight options breaks the map
+    
+    leaflet(printPlot) %>%
+      addPolygons(
+        stroke = TRUE,
+        fillOpacity = 0.1, smoothFactor = 0.9,
+        highlightOptions = highlightOptions(
+          weight = 5,
+          color = "#600",
+          fillOpacity = 0.7,
+          bringToFront = TRUE),
+        label = labels,
+        labelOptions = labelOptions(
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "15px",
+          direction = "auto")
+      ) %>%
+      addTiles() %>%  
+      setView(lng =-87.658323, lat = 41.879036, zoom = 12) %>%
+      addProviderTiles("Esri.WorldGrayCanvas")
+    
+    
+  })
 }
 shinyApp(ui, server)
 
